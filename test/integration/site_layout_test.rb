09118763f0e47bda13a605d8194b363298ac8282
EJTang <ejtang@ualberta.ca>
2017-09-18 07:36:05 +0200

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -11,5 +11,16 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+
+    @user = users(:micheal)
+    log_in_as(@user)
+    assert_redirected_to @user
+    follow_redirect!
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", user_path
+    assert_select "a[href=?]", edit_user_path
+    assert_select "a[href=?]", logout_path
   end
 end
